9822d77fe19a9774ae3e65c109913cac1179567a
Nick Porsch <porschnickalas@gmail.com>
2023-03-22 13:38:46 -0500

    Made it so you can change the new line character.

--- TCP Connector/tcpConnector.py
+++ TCP Connector/tcpConnector.py
@@ -4,17 +4,21 @@ import socket
 class TCPConnector:
 
     sock = None
+    delim = "\n"
 
     def __init__(self,hostname, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((hostname, port))
+
+    def setDelimiter(self,value):
+        self.delim = value
 
     def getRequest(self):
         data = self.sock.recv(2048)
         return data.decode()
 
     def sendRequest(self, value):
-        value += "\n"
+        value += self.delim
         self.sock.sendall(value.encode())
         return value
     
