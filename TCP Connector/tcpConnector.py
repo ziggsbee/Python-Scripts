dcc7efc21f1fa2ab7c11e298fa27582aec61aefe
Nick Porsch <porschnickalas@gmail.com>
2023-03-22 13:36:16 -0500

    cleaned it up a little more

--- TCP Connector/tcpConnector.py
+++ TCP Connector/tcpConnector.py
@@ -6,16 +6,12 @@ class TCPConnector:
     sock = None
 
     def __init__(self,hostname, port):
-
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((hostname, port))
 
     def getRequest(self):
-        full_message = ""
         data = self.sock.recv(2048)
-        full_message += data.decode()
-
-        return full_message
+        return data.decode()
 
     def sendRequest(self, value):
         value += "\n"
